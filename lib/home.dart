07135339501e0a1b6819a6ddafa1f291dbacbc4b
DiegoVanegas03 <gamesonfn@gmail.com>
2024-02-08 14:23:15 -0600

    Clase 6 de febrero 2024

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:app2/constantes.dart' as constantes;
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,11 +14,54 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: Stack(
         children: [
-          Center(
-            child: Text('BIENVENIDO OTRA VEZ'),
+          Column(
+            children: [
+              CardPersonalizate(txt:'Personalizado 1', size: 20, showOrHide: false,),
+              CardPersonalizate(txt:'Personalizado 2',size: 25, showOrHide: true),
+              CardPersonalizate(txt:'Personalizado 3',size: 7, showOrHide: true),
+            ],
           )
         ],
       ),
+    );
+  }
+}
+
+class CardPersonalizate extends StatelessWidget {
+  const CardPersonalizate({
+    super.key,
+    required this.txt,
+    required this.size,
+    required this.showOrHide,
+  });
+
+  final String txt;
+  final double size;
+  final bool showOrHide;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: constantes.colorPrincipal,
+      elevation: 10,
+      child: Padding(
+          padding: EdgeInsets.all(size),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 6,
+                child:  Text(txt),
+              ),
+              Expanded(
+                flex: 4,
+                child: Container(color: Colors.white, height: 20,),
+              ),
+              Expanded(
+                flex: 2,
+                child: showOrHide ? const Icon(Icons.sms) : Container(),
+              ),
+            ],
+          ),
+        )
     );
   }
 }
